ddd811f08e886a3fede288702cb27728356940bf
Shu Wei Wu <wushuwei@gmail.com>
2021-03-15 23:40:22 -0400

    add pattern recognition

--- blocks/main/main.go
+++ blocks/main/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+	
 
 	"github.com/wushuwei/mongo/blocks/routines"
 )
 
 func main() {
 
-	fmt.Print(utilities.Double(3))
+	// fmt.Print(utilities.Double(3))
 	// utilities.ConnetMongo()
 	utilities.ConnetFinnhub()
 }
